92f732062d13cd00d2941bd5585faf66b2569100
kalaiselvankadirvel <kalaiselvan@outlook.com>
2019-07-05 20:26:18 -0400

    Modified brand name

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -13,7 +13,7 @@ class HeaderComponent extends Component {
         return (
             <header>
                 <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-                    <div><a href="http://www.in28minutes.com" className="navbar-brand">in28Minutes</a></div>
+                    <div><a href="http://www.in28minutes.com" className="navbar-brand">kalaiselvan.in</a></div>
                     <ul className="navbar-nav">
                         {isUserLoggedIn && <li><Link className="nav-link" to="/welcome/admin">Home</Link></li>}
                         {isUserLoggedIn && <li><Link className="nav-link" to="/todos">Todos</Link></li>}
